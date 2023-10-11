795ea3fb9c04e249a2458ac5af07104c93957d6c
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-11 15:56:53 +0530

    converting csv

---
+++ src/main/java/com/cricket/IPLdashboard/IpLdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.IPLdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpLdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/IPLdashboard/IpLdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.IPLdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
