df4fa88e5b453bf7a7a2d79bdf9b720f3d5d21b3
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-11 17:14:38 +0530

    data converting done

---
+++ src/main/java/com/cricket/IPLdashboard/model/Match.java
@@ -0,0 +1,34 @@
+package com.cricket.IPLdashboard.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Match {
+    @Id
+    private long id;
+    private String city;
+    private LocalDate date;
+    private String playerOfMatch;
+    private String venue;
+    private String neutral_venue;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String result;
+    private String resultMargin;
+//    private String eliminator;
+//    private String method;
+    private String umpire1;
+    private String umpire2;
+}
