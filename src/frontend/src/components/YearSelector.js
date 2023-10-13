81e2fa2336ba56ea173fd113d13e2ce740cafa9c
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-14 03:36:57 +0530

    cleaning up

--- src/frontend/src/components/YearSelector.js
+++ src/frontend/src/components/YearSelector.js
@@ -6,10 +6,9 @@ const YearSelector = ({teamName}) => {
     const years=[];
     const startYear=process.env.REACT_APP_DATA_START_YEAR;
     const endYear=process.env.REACT_APP_DATA_END_YEAR;
-console.log("abc:",startYear,endYear);
+
     for(let i=startYear;i<=endYear;i++){
         years.push(i);
-        console.log(i)
     }
 
     return ( 
