4f508e1b5e001b9f19427f585e44ce0c37f2e15a
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-13 23:13:43 +0530

    styles and install pie chart

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,14 +1,25 @@
-import {Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+
+import "./MatchSmallCard.scss"
 
 const MatchSmallCard = ({ match, teamName }) => {
   if (!match) return null;
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
-const otherTeamRoute=`/teams/${otherTeam}`;
+  const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
 
   return (
-    <div className="MatchSmallCard">
-      <Link to={otherTeamRoute}><h3> vs {otherTeam}</h3></Link>
-      <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
+    <div
+      className={
+        isMatchWon ? "MatchSmallCard won-card" : "MatchSmallCard lost-card"
+      }
+    >
+      <span className="vs">vs</span>
+          <h3><Link to={otherTeamRoute}>{otherTeam}</Link></h3>
+      
+      <p className="match-result">
+        {match.matchWinner} won by {match.resultMargin} {match.result}
+      </p>
     </div>
   );
 };
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,6 +1,8 @@
 import { useEffect,useState } from "react";
 import { useParams } from 'react-router-dom';
 
+import './TeamPage.scss';
+
 import MatchDetailCard from "../components/MatchDetailCard";
 import MatchSmallCard from "../components/MatchSmallCard";
 
@@ -27,10 +29,19 @@ const TeamPage = () => {
     }
     return ( 
         <div className="TeamPage">
-            <h1>{team.teamName}</h1>
+            <div className="team-name-section">
+            <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            <div className="win-loss-section">Wins / Losses</div>
+            <div className="match-detail-section">
+            <h3>Latest Matches</h3>
             <MatchDetailCard teamName={team.teamName} match={team.matches[0]}></MatchDetailCard>
+            </div>
             {team.matches.slice(1).map((match)=> <MatchSmallCard key={match.id} teamName={team.teamName} match={match}></MatchSmallCard>)}
 
+            <div className="more-link">
+                <a href="asdas">More> </a>
+            </div>
         </div>
      );
 }
