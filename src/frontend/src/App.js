1a6d8571ca6e59e8e1f47c252ef2d4c60cb7be0e
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-13 11:44:17 +0530

    match page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import TeamPage from './pages/TeamPage';
+import "./App.css";
+import MatchPage from "./pages/MatchPage";
+import TeamPage from "./pages/TeamPage";
 // import {BrowserRouter  as Router} from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -13,13 +13,16 @@ function App() {
       <Router  path="/teamsd/:teamName">
         <TeamPage></TeamPage>
       </Router> */}
-      
+
       <Router>
-      <Routes>
-        {/* <Route path="/" exact element={<h1>sadsdads </h1>}/> */}
-        <Route path="/teams/:teamName" element={<TeamPage/>} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          />
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
