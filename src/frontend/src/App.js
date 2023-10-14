149d614d854f2e25f6321a3b997ca5bd0cc1cd8d
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-15 00:54:29 +0530

    refresh error fix

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,7 +3,7 @@ import MatchPage from "./pages/MatchPage";
 import TeamPage from "./pages/TeamPage";
 import HomePage from "./pages/HomePage";
 // import {BrowserRouter  as Router} from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
